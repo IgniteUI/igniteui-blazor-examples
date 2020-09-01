dad8b31bda29f988a67c1c7c609a6b96557a762b
HUSSAR-mtrela <martin.trela@gmail.com>
2020-08-31 23:32:45 -0400

    updated IG Packages in browser

--- browser/IgBlazorSamples.Gulp/tasks/Transformer.ts
+++ browser/IgBlazorSamples.Gulp/tasks/Transformer.ts
@@ -436,7 +436,7 @@ class Transformer {
 
             for (let pack of igConfig.PackageReferences) {
                 if (Strings.contains(line, pack.Name)) {
-                    //     <PackageReference Include="Infragistics.Blazor" Version="20.1.73-dev" />
+                    //     <PackageReference Include="Infragistics.Blazor" Version="20.1.7" />
                     line = '    <PackageReference Include="' + pack.Name + '" Version="' + pack.Version+ '" />'
                     console.log("updating " + projectFile.Path + " with " + pack.Name);
                     break;
