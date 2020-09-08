c5ad124df9677713bdb02b9f85585444ea5190fd
HUSSAR-mtrela <martin.trela@gmail.com>
2020-09-08 12:05:44 -0400

    updated packages to v20.1.84-dev

--- browser/IgBlazorSamples.Gulp/tasks/Transformer.ts
+++ browser/IgBlazorSamples.Gulp/tasks/Transformer.ts
@@ -436,7 +436,7 @@ class Transformer {
 
             for (let pack of igConfig.PackageReferences) {
                 if (Strings.contains(line, pack.Name)) {
-                    //     <PackageReference Include="Infragistics.Blazor" Version="20.1.7" />
+                    //     <PackageReference Include="Infragistics.Blazor" Version="20.1.84-dev" />
                     line = '    <PackageReference Include="' + pack.Name + '" Version="' + pack.Version+ '" />'
                     console.log("updating " + projectFile.Path + " with " + pack.Name);
                     break;
--- browser/IgBlazorSamples.Gulp/tasks/gulp-config.js
+++ browser/IgBlazorSamples.Gulp/tasks/gulp-config.js
@@ -39,7 +39,7 @@ let config = {
     SandboxUrlShort: "https://codesandbox.io/embed/{RepositoryPath}",
 
     PackageReferences: [
-        { Name: "Infragistics.Blazor", Version: "20.1.7"},
+        { Name: "Infragistics.Blazor", Version: "20.1.84-dev"},
     ],
 }
 
