1b5c196c048c8c93330e7e18e564bef8d71db9b3
agoldenbaum <agoldenbaum@infragistics.com>
2020-09-21 16:43:18 -0400

    Check in DataChart "Series" samples

---
+++ samples/charts/data-chart/series-value-overlay/Services/DataChartSharedData.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+
+namespace Infragistics.Samples
+{
+    public static class DataChartSharedData
+    {
+        public class DataItem {
+
+            public string Label { get; set; }
+            public double Value { get; set; }
+        }       
+    }
+}
