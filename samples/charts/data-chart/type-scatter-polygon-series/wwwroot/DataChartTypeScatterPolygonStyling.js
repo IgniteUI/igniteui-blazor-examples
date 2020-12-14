f4b4c957b13c9b73dec3b4a5ebe5bbc5afade779
HUSSAR-mtrela <martin.trela@gmail.com>
2020-12-14 15:30:59 -0500

    replaced data in shape samples with airplane json

---
+++ samples/charts/data-chart/type-scatter-polygon-series/wwwroot/DataChartTypeScatterPolygonStyling.js
@@ -0,0 +1,67 @@
+﻿
+function onStylingShape(o, e) {
+
+    e.shapeOpacity = 1.0;
+    e.shapeStrokeThickness = 0.5;
+    e.shapeStroke = 'Black';
+
+    var dataItem = e.item;
+    if (dataItem.Class === 'First') {
+        e.shapeFill = 'DodgerBlue';
+    }
+    if (dataItem.Class === 'Business') {
+        e.shapeFill = 'LimeGreen';
+    }
+    if (dataItem.Class === 'Premium') {
+        e.shapeFill = 'Orange';
+    }
+    if (dataItem.Class === 'Economy') {
+        e.shapeFill = 'Red';
+    }
+
+    if (dataItem.Status === 'Sold') {
+        e.shapeFill = 'Gray';
+    }
+}
+igRegisterScript("onStylingShape", onStylingShape, false);
+
+function onSeriesMouseEnter(o, e) {
+    if (e.series.title !== "AirplaneSeats") return;
+
+    if (e.series.tooltipTemplate === null ||
+        e.series.tooltipTemplate === undefined) {
+        e.series.tooltipTemplate = createTooltip;
+        console.log("onSeriesMouseEnter createTooltip ");
+    }
+}
+igRegisterScript("onSeriesMouseEnter", onSeriesMouseEnter, false);
+
+function createTooltip(context) {
+
+    if (!context) return null;
+
+    var dataItem = context.item;
+    if (!dataItem) return null;
+
+    var tooltip = document.createElement("div");
+    tooltip.className = "ui-tooltip-content";
+
+    var itemClass = document.createElement("div");
+    itemClass.innerHTML = "Class: " + dataItem.Class;
+    //itemClass.style.color = "black";
+    tooltip.appendChild(itemClass);
+
+    var itemSeat = document.createElement("div");
+    itemSeat.innerHTML = "Seat: " + dataItem.Seat; 
+    tooltip.appendChild(itemSeat);
+
+    var itemPrice = document.createElement("div");
+    itemPrice.innerHTML = "Price: $" + dataItem.Price;
+    tooltip.appendChild(itemPrice);
+    
+    var itemStatus = document.createElement("div");
+    itemStatus.innerHTML = "Status: " + dataItem.Status;
+    tooltip.appendChild(itemStatus);
+     
+    return tooltip;
+}
