dd3c108aa7b7f970b147834b3f93bbb3cd44f049
Nicholas Parr <48603808+parrnicholasj@users.noreply.github.com>
2020-12-28 15:46:38 -0500

    adding mapmarkertemplate sample
    the markertemplatescript on line 27 of the mapmarkertemplate.razor page does not work and seems to not be implemented

---
+++ samples/maps/geo-map/marker-template/Services/WorldCity.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Infragistics.Samples
+{
+    public class WorldCity
+    {
+        public string Name { get; set; }
+
+        public double Lat { get; set; }
+
+        public double Lon { get; set; }
+
+        public double Pop { get; set; }
+
+        public string Country { get; set; }
+        public bool Cap { get; set; }
+
+    }
+}
