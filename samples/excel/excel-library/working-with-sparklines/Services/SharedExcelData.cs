9b6714459c5a7436aa30961563815477a4553b6e
HUSSAR-mtrela <martin.trela@gmail.com>
2021-02-15 12:34:13 -0500

    synced excel data

--- samples/excel/excel-library/working-with-sparklines/Services/SharedExcelData.cs
+++ samples/excel/excel-library/working-with-sparklines/Services/SharedExcelData.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Rendering;
 using Microsoft.AspNetCore.Components.Forms;
 using Microsoft.AspNetCore.Components.RenderTree;
