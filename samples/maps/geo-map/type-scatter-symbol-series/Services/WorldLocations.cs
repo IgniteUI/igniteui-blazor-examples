e2589878c3e85312a173f59ab858324d52a953cf
HUSSAR-mtrela <martin.trela@gmail.com>
2021-02-15 12:27:37 -0500

    synced WorldLocations.cs

--- samples/maps/geo-map/type-scatter-symbol-series/Services/WorldLocations.cs
+++ samples/maps/geo-map/type-scatter-symbol-series/Services/WorldLocations.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -637,6 +637,6 @@ namespace Infragistics.Samples
             };
 
             return cities;
-        }           
+        }
     }
 }
