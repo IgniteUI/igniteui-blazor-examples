9cb1d1b8eaa5a065b1e833f8939db61a8f95027a
HUSSAR-mtrela <martin.trela@gmail.com>
2021-03-22 22:06:24 -0400

    fixed network polar chart

--- samples/charts/data-chart/network-polar-chart/wwwroot/DataChartNetworkPolarChartScripts.js
+++ samples/charts/data-chart/network-polar-chart/wwwroot/DataChartNetworkPolarChartScripts.js
@@ -29,17 +29,12 @@ function onNetworkPolarChartTooltip(context) {
     div.innerHTML = "Level: " + dataItem.Radius;
     tooltip.appendChild(div);
 
-    div = document.createElement("div");
-    div.innerHTML = "Angle: " + dataItem.Angle;
-    //itemClass.style.color = "black";
-   // tooltip.appendChild(div);
-
     return tooltip;
 }
 
 function onNetworkPolarChartMarker(o, e) {
 
-    var desiredSize = 46;
+    var desiredSize = 45;
     return {
         measure: function (measureInfo) {
             measureInfo.width = desiredSize;
@@ -71,47 +66,25 @@ function onNetworkPolarChartMarker(o, e) {
                 var data = renderInfo.data;
 
                 var viewportHeight = renderInfo.passInfo.viewportHeight;
-                var viewportRatio = viewportHeight / 200.0;// 150.0;
-                var markerSize = viewportRatio * desiredSize;
-                var markerHalf = markerSize / 2.0;
+                var viewportRatio = viewportHeight / 200.0; 
+                var markerSize = viewportRatio * desiredSize; 
+                var markerRadius = Math.min(50, data.item.Radius);
+                ctx.strokeStyle = data.item.Outline; 
+                ctx.fillStyle   = data.item.Fill;  
+                ctx.lineWidth   = data.item.Thickness; 
+                ctx.beginPath(); 
+                ctx.arc(cx, cy, markerRadius, 0, 2 * Math.PI);
+                ctx.stroke();
+                ctx.fill(); 
 
-              //  if (data.item.IsCircle) {
-
-                 //   var markerRadius = Math.min(150, data.item.Radius);
-                    var actualRadius = viewportRatio * data.item.Radius;
-                   // var markerRadius = Math.min(150, viewportRatio * data.item.Radius);
-                    var markerRadius = Math.min(50, data.item.Size);
-                   
-                    ctx.strokeStyle = data.item.Outline; //"black";
-                    ctx.fillStyle   = data.item.Fill; //"rgba(193, 66, 66, 0.2)";
-                    ctx.lineWidth   = data.item.Thickness;
-                    // ctx.fillStyle = "rgba(193, 66, 66, 0)";
-                    ctx.beginPath();
-                    //ctx.arc(cx, cy, markerHalf, 0, 2 * Math.PI);
-                   // ctx.arc(cx, cy, markerSize, 0, 2 * Math.PI);
-                    ctx.arc(cx, cy, markerRadius, 0, 2 * Math.PI);
-                    ctx.stroke();
-                    ctx.fill();
-                    //ctx.globalAlpha = 0.4;
-               // }
-                //else {
-
-                    var text = data.item.Label.toString();
-                    // var text = data.item.Value.toString();
-                    // var text = data.item.Total.toString();
-                    //  var text = data.item.LabelText.toString() + " " + data.item.Value.toString();
-
-                var textSize = Math.max(10, data.item.LabelSize); // Math.round(viewportRatio * 10);
+                var text = data.item.Label.toString();
+                // var textSize = Math.round(viewportRatio * 10);
+                var textSize = Math.max(10, data.item.LabelSize); 
                 ctx.font = "normal " + textSize + "px Verdana";
-                    ctx.textBaseline = "middle";
-                    ctx.textAlign = "center";
-                    ctx.fillStyle = data.item.LabelColor; //"black";
-                    //ctx.fillStyle = "white";
-                    ctx.fillText(text, cx, cy);
-                /*}*/
-                //ctx.fillStyle = "#30A510";
-                //ctx.fillRect(x, y, renderInfo.availableWidth, renderInfo.availableHeight);
-
+                ctx.textBaseline = "middle";
+                ctx.textAlign = "center";
+                ctx.fillStyle = data.item.LabelColor; 
+                ctx.fillText(text, cx, cy);
             }
         }
     }
