347f2425ac0def7f4ddb222c17caf5b05f1dcc1a
Michael DiFilippo <mdifilippo@infragistics.com>
2021-04-08 22:29:03 -0400

    mdd-seo-tooltip
    mdd-seo-tooltip

---
+++ samples/charts/data-chart/tooltip-template/wwwroot/DataChartTooltipStyles.js
@@ -0,0 +1,47 @@
+﻿console.log("loaded DataChartTooltipStyles.js");
+
+function onColumnSeriesMouseEnter(o, e) {
+    if (e.series.tooltipTemplate === null ||
+        e.series.tooltipTemplate === undefined) {
+        e.series.tooltipTemplate = createTooltip;
+        console.log("onColumnSeriesMouseEnter");
+    }
+}
+igRegisterScript("onColumnSeriesMouseEnter", onColumnSeriesMouseEnter, false);
+
+function createTooltip(context) {
+
+    if (!context) return null;
+
+    var dataItem = context.item;
+    if (!dataItem) return null;
+
+    var tooltip = document.createElement("div");
+    tooltip.className = "ui-tooltip-content";
+
+    var itemCountry = document.createElement("div");
+    itemCountry.innerHTML = "Class: " + dataItem.Country;
+    tooltip.appendChild(itemCountry);
+
+    var itemCoal = document.createElement("div");
+    itemCoal.innerHTML = "Coal: " + dataItem.Coal;
+    tooltip.appendChild(itemCoal);
+
+    var itemOil = document.createElement("div");
+    itemOil.innerHTML = "Oil: " + dataItem.Oil;
+    tooltip.appendChild(itemOil);
+
+    var itemGas = document.createElement("div");
+    itemGas.innerHTML = "Gas: " + dataItem.Gas;
+    tooltip.appendChild(itemGas);
+
+    var itemNuclear = document.createElement("div");
+    itemNuclear.innerHTML = "Nuclear: " + dataItem.Nuclear;
+    tooltip.appendChild(itemNuclear);
+
+    var itemHydro = document.createElement("div");
+    itemHydro.innerHTML = "Hydro: " + dataItem.Hydro;
+    tooltip.appendChild(itemHydro);
+
+    return tooltip;
+}
