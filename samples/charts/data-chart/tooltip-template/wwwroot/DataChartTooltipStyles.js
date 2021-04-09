c4eeae16cb2568979ca1d38a57cc374542801f86
HUSSAR-mtrela <martin.trela@gmail.com>
2021-04-09 11:34:14 -0400

    fixed custom tooltip

--- samples/charts/data-chart/tooltip-template/wwwroot/DataChartTooltipStyles.js
+++ samples/charts/data-chart/tooltip-template/wwwroot/DataChartTooltipStyles.js
@@ -10,38 +10,59 @@ function onColumnSeriesMouseEnter(o, e) {
 igRegisterScript("onColumnSeriesMouseEnter", onColumnSeriesMouseEnter, false);
 
 function createTooltip(context) {
-
     if (!context) return null;
 
     var dataItem = context.item;
     if (!dataItem) return null;
 
+    var dataSeries = context.series;
+    if (!dataSeries) return null;
+
     var tooltip = document.createElement("div");
     tooltip.className = "ui-tooltip-content";
 
-    var itemCountry = document.createElement("div");
-    itemCountry.innerHTML = "Class: " + dataItem.Country;
-    tooltip.appendChild(itemCountry);
+    var title = document.createElement("div");
+    title.innerHTML = dataItem.Country + " Production";
+    title.className = "tooltipTitle";
+    tooltip.appendChild(title);
 
-    var itemCoal = document.createElement("div");
-    itemCoal.innerHTML = "Coal: " + dataItem.Coal;
-    tooltip.appendChild(itemCoal);
+    var line1 = createTooltipLine(dataSeries, "Coal", dataItem.Coal);
+    var line2 = createTooltipLine(dataSeries, "Hydro", dataItem.Hydro);
+    var line3 = createTooltipLine(dataSeries, "Nuclear", dataItem.Nuclear);
+    var line4 = createTooltipLine(dataSeries, "Gas", dataItem.Gas);
+    var line5 = createTooltipLine(dataSeries, "Oil", dataItem.Oil);
 
-    var itemOil = document.createElement("div");
-    itemOil.innerHTML = "Oil: " + dataItem.Oil;
-    tooltip.appendChild(itemOil);
-
-    var itemGas = document.createElement("div");
-    itemGas.innerHTML = "Gas: " + dataItem.Gas;
-    tooltip.appendChild(itemGas);
-
-    var itemNuclear = document.createElement("div");
-    itemNuclear.innerHTML = "Nuclear: " + dataItem.Nuclear;
-    tooltip.appendChild(itemNuclear);
-
-    var itemHydro = document.createElement("div");
-    itemHydro.innerHTML = "Hydro: " + dataItem.Hydro;
-    tooltip.appendChild(itemHydro);
+    tooltip.appendChild(line1);
+    tooltip.appendChild(line2);
+    tooltip.appendChild(line3);
+    tooltip.appendChild(line4);
+    tooltip.appendChild(line5);
 
     return tooltip;
+}
+
+function createTooltipLine(dataSeries, dataName, dataValue) {
+
+    var label = document.createElement("label");
+    label.innerHTML = dataName + ":";
+    label.className = "tooltipLbl";
+    label.style.width = "4rem";
+
+    var value = document.createElement("label");
+    value.innerHTML = dataValue;
+    value.className = "tooltipVal";
+
+    var line = document.createElement("div");
+    line.className = "tooltipHorizontal";
+
+    // applying conditional styling based on mapping of the current series
+    var isMatching = dataSeries.valueMemberPath == dataName;
+    if (isMatching)
+        line.style.color = dataSeries.actualBrush;
+    else
+        line.style.color = "black";
+
+    line.appendChild(label);
+    line.appendChild(value);
+    return line;
 }
