4fa8d5acfe47f0e3b711206b832f37b42693970f
HUSSAR-mtrela <martin.trela@gmail.com>
2021-09-21 18:21:47 -0400

    corrected batch script

--- browser/cleanup.bat
+++ browser/cleanup.bat
@@ -1,16 +1,33 @@
 @echo off
 REM ECHO cleanup %cd% ...
-FOR %%I IN (.) DO set TARGET=%%~nxI
-ECHO cleanup %TARGET% ...
+REM FOR %%I IN (.) DO set TARGET=%%~nxI
+REM ECHO cleanup %TARGET%\bin ...
+ 
+ECHO cleanup browser's folders ...
+IF EXIST IgBlazorSamples.Client\bin\ (
+   RMDIR /S /Q IgBlazorSamples.Client\bin
+)  
+IF EXIST IgBlazorSamples.Client\obj\ (
+   RMDIR /S /Q IgBlazorSamples.Client\obj
+)  
 
-ECHO - removing %TARGET%\**\BIN folders in all samples:
-REM FOR /F "tokens=*" %%G IN ('DIR /B /AD /S bin*') DO echo "%%G"
-    FOR /F "tokens=*" %%G IN ('DIR /B /AD /S bin') DO RMDIR /S /Q "%%G"
+REM RMDIR /S /Q IgBlazorSamples.Client\bin
+REM RMDIR /S /Q IgBlazorSamples.Client\obj
+REM RMDIR /S /Q IgBlazorSamples.Core\bin
+REM RMDIR /S /Q IgBlazorSamples.Core\obj
+REM RMDIR /S /Q IgBlazorSamples.Server\bin
+REM RMDIR /S /Q IgBlazorSamples.Server\obj
+REM RMDIR /S /Q IgBlazorSamples.Shared\bin
+REM RMDIR /S /Q IgBlazorSamples.Shared\obj
 
-ECHO - removing %TARGET%\**\OBJ folders in all samples:
-FOR /F "tokens=*" %%G IN ('DIR /B /AD /S obj') DO RMDIR /S /Q "%%G"
+REM ECHO - removing %TARGET%\**\BIN folders in all samples:
+REM REM FOR /F "tokens=*" %%G IN ('DIR /B /AD /S bin*') DO echo "%%G"
+REM     FOR /F "tokens=*" %%G IN ('DIR /B /AD /S bin') DO RMDIR /S /Q "%%G"
 
-ECHO cleanup %TARGET% completed.
+REM ECHO - removing %TARGET%\**\OBJ folders in all samples:
+REM FOR /F "tokens=*" %%G IN ('DIR /B /AD /S obj') DO RMDIR /S /Q "%%G"
+
+ECHO cleanup browser's folders completed.
 pause
 
  
