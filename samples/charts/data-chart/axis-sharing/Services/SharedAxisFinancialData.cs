76a628eb30722cfed80873be093f20685d6de7c6
HUSSAR-mtrela <martin.trela@gmail.com>
2021-11-23 11:43:57 -0500

    fixed shared axis sample

--- samples/charts/data-chart/axis-sharing/Services/SharedAxisFinancialData.cs
+++ samples/charts/data-chart/axis-sharing/Services/SharedAxisFinancialData.cs
@@ -3,12 +3,12 @@ using System.Collections.Generic;
 
 namespace Infragistics.Samples
 {
-    public class SampleFinancialData
+    public class SharedAxisFinancialData
     {
         public static Random random = new Random();
-        public static List<SampleFinancialItem> Create(int itemsCount = 365)
+        public static List<SharedAxisFinancialItem> Create(int itemsCount = 365)
         {
-            var data = new List<SampleFinancialItem>();
+            var data = new List<SharedAxisFinancialItem>();
            
             // initial values
             var v = 10000.0;
@@ -18,7 +18,7 @@ namespace Infragistics.Samples
             var c = Math.Round(l + (random.NextDouble() * (h - l)));
 
             var today = DateTime.Now;
-            var end = new DateTime(today.Year, 12, 31);
+            var end = new DateTime(today.Year, today.Month, today.Day);
             var time = end.AddDays(-itemsCount);
 
             for (var i = 0; i < itemsCount; i++)
@@ -26,7 +26,7 @@ namespace Infragistics.Samples
                 var date = time.ToShortDateString();
                 var label = GetShortDate(time, false);
                 // adding new data item
-                var item = new SampleFinancialItem();
+                var item = new SharedAxisFinancialItem();
                 item.Time = time;
                 item.Date = date;
                 item.Label = label;
@@ -36,10 +36,13 @@ namespace Infragistics.Samples
                 item.Low = l;
                 item.Volume = v;
                 data.Add(item);
-
+                
                 // generating new values
                 var mod = random.NextDouble() - 0.49;
                 o = Math.Round(o + (mod * 5 * 4));
+                o = Math.Max(o, 500);
+                o = Math.Min(o, 675);
+
                 v = Math.Round(v + (mod * 5 * 100));
                 h = Math.Round(o + (random.NextDouble() * 15));
                 l = Math.Round(o - (random.NextDouble() * 15));
@@ -65,7 +68,7 @@ namespace Infragistics.Samples
         }
     }
 
-    public class SampleFinancialItem
+    public class SharedAxisFinancialItem
     {
         public double High { get; set; }
         public double Low { get; set; }
