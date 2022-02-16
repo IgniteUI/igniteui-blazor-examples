5e92fd7966f42a6dd30444d4af8c0d23a9f02fbb
HUSSAR-mtrela <martin.trela@gmail.com>
2022-02-16 15:52:51 -0500

    fixed app RootComponents

--- browser/IgBlazorSamples.Client/Program.cs
+++ browser/IgBlazorSamples.Client/Program.cs
@@ -7,7 +7,6 @@ using System.Net.Http;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using System.Text;
-using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
@@ -16,7 +15,7 @@ using IgniteUI.Blazor.Controls; // provides IgniteUIBlazor
 
 // copied from Blazor .NET 6 project:
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
-builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<App>("app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 // registering local browser components
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
