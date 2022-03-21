81631848da32297824939af67949abf8a81915b5
Michael DiFilippo <mdifilippo@infragistics.com>
2022-03-21 16:02:32 -0400

    mdd-update-to-build-50
    21.2.50

--- browser/IgBlazorSamples.Gulp/tasks/gulp-config.js
+++ browser/IgBlazorSamples.Gulp/tasks/gulp-config.js
@@ -39,7 +39,7 @@ let config = {
     SandboxUrlShort: "https://codesandbox.io/embed/{RepositoryPath}",
 
     PackageReferences: [
-        { Name: "Infragistics.Blazor", Version: "21.2.47"},
+        { Name: "Infragistics.Blazor", Version: "21.2.50"},
     ],
 }
 
