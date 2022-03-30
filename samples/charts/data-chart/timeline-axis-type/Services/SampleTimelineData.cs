64740fe7f62d52964a99b0e1f2f0acf130783532
Michael DiFilippo <mdifilippo@infragistics.com>
2022-03-30 14:32:51 -0400

    mdd-update-timeline
    update timeline

---
+++ samples/charts/data-chart/timeline-axis-type/Services/SampleTimelineData.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Infragistics.Samples
+{
+    public class SampleTimelineData
+    {
+        public static List<SampleTimelineItem> Create() {
+            // total olympic medals for top countries
+            var data = new List<SampleTimelineItem>() {
+                new SampleTimelineItem {Year = "23 June, 2016", Date = new DateTime(2016, 6, 23),   Y = 5, Details = "UK votes to exit the EU"},
+                new SampleTimelineItem {Year = "29 March, 2017", Date = new DateTime(2017, 3, 29), Y = 5,  Details = "The UK triggers Article 50"},
+                new SampleTimelineItem {Year = "19 June, 2017", Date = new DateTime(2017, 6, 19),  Y = 5, Details = "Brexit negotiations begin"},
+                new SampleTimelineItem {Year = "19 Marhc, 2018", Date = new DateTime(2018, 3, 19), Y = 5,  Details = "The EU and the UK agree on a transition phase"},
+                new SampleTimelineItem {Year = "25 November, 2018", Date = new DateTime(2018, 12, 25),  Y = 5, Details = "Draft withdrawl deal agreed"},
+                new SampleTimelineItem {Year = "29 October, 2019", Date = new DateTime(2019, 10, 29),Y = 5,  Details = "EU heads of state and government approve postponing the Brexit date"},
+
+            return data;
+        }
+    }
+
+    public class SampleTimelineItem
+    {
+        public string Details { get; set; }
+        public int X { get; set; }
+        public int Y { get; set; }
+        public string Year { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
