c84c8a58b5f1f902c7a73ed8e7dc55d0a501fa1f
HUSSAR-mtrela <HUSSAR-mtrela@users.noreply.github.com>
2022-04-05 15:23:53 -0400

    Update SampleTimelineData.cs

--- samples/charts/data-chart/timeline-axis-type/Services/SampleTimelineData.cs
+++ samples/charts/data-chart/timeline-axis-type/Services/SampleTimelineData.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 
 namespace Infragistics.Samples
@@ -6,16 +6,14 @@ namespace Infragistics.Samples
     public class SampleTimelineData
     {
         public static List<SampleTimelineItem> Create() {
-            // total olympic medals for top countries
             var data = new List<SampleTimelineItem>() {
-                new SampleTimelineItem {Year = "23 June, 2016", Date = new DateTime(2016, 6, 23),   Y = 5, Details = "UK votes to exit the EU"},
-                new SampleTimelineItem {Year = "29 March, 2017", Date = new DateTime(2017, 3, 29), Y = 5,  Details = "The UK triggers Article 50"},
-                new SampleTimelineItem {Year = "19 June, 2017", Date = new DateTime(2017, 6, 19),  Y = 5, Details = "Brexit negotiations begin"},
-                new SampleTimelineItem {Year = "19 March, 2018", Date = new DateTime(2018, 3, 19), Y = 5,  Details = "The EU and the UK agree on a transition phase"},
-                new SampleTimelineItem {Year = "25 November, 2018", Date = new DateTime(2018, 12, 25),  Y = 5, Details = "Draft withdrawl deal agreed"},
-                new SampleTimelineItem {Year = "29 October, 2019", Date = new DateTime(2019, 10, 29),Y = 5,  Details = "EU heads of state and government approve postponing the Brexit date"},
-                new SampleTimelineItem {Year = "31 December, 2020", Date = new DateTime(2020, 12, 31), Y = 5, Details = "Transition period ends"},
-
+                new SampleTimelineItem { Year = "23 June, 2016", Date = new DateTime(2016, 6, 23),   Y = 5, Details = "UK votes to exit the EU"},
+                new SampleTimelineItem { Year = "29 March, 2017", Date = new DateTime(2017, 3, 29), Y = 5,  Details = "The UK triggers Article 50"},
+                new SampleTimelineItem { Year = "19 June, 2017", Date = new DateTime(2017, 6, 19),  Y = 5, Details = "Brexit negotiations begin"},
+                new SampleTimelineItem { Year = "19 March, 2018", Date = new DateTime(2018, 3, 19), Y = 5,  Details = "The EU and the UK agree on a transition phase"},
+                new SampleTimelineItem { Year = "25 November, 2018", Date = new DateTime(2018, 12, 25),  Y = 5, Details = "Draft withdrawl deal agreed"},
+                new SampleTimelineItem { Year = "29 October, 2019", Date = new DateTime(2019, 10, 29),Y = 5,  Details = "EU heads of state and government approve postponing the Brexit date"},
+                new SampleTimelineItem { Year = "31 December, 2020", Date = new DateTime(2020, 12, 31), Y = 5, Details = "Transition period ends"},
             };
             return data;
         }
