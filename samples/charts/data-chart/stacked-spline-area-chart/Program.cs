61ba63942811b7ad30e796c9f715fa5c449cb3bf
HUSSAR-mtrela <martin.trela@gmail.com>
2022-06-17 16:56:04 -0400

    fixes samples

--- samples/charts/data-chart/stacked-spline-area-chart/Program.cs
+++ samples/charts/data-chart/stacked-spline-area-chart/Program.cs
@@ -24,6 +24,7 @@ namespace Infragistics.Samples
 
             // registering Infragistics Blazor
             builder.Services.AddIgniteUIBlazor(
+                typeof(IgbLegendModule), 
                 typeof(IgbDataChartCoreModule), 
                 typeof(IgbDataChartCategoryModule), 
                 typeof(IgbDataChartCategoryCoreModule), 
