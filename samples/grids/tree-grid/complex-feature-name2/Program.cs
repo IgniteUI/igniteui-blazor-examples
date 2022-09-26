c584a36414209cb14cec0db0f18b45d6e3e36a7d
mtrela <mtrela@infragistics.com>
2022-09-26 10:54:47 -0400

    added grid template samples

---
+++ samples/grids/tree-grid/complex-feature-name2/Program.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Net.Http;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Text;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+// required for registering IgniteUIBlazor
+using IgniteUI.Blazor.Controls;
+
+namespace Infragistics.Samples
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+            builder.RootComponents.Add<App>("app");
+
+            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+            // TODO update names of the following modules for the IgbTreeGrid
+            builder.Services.AddIgniteUIBlazor(
+                typeof(IgbDataGridModule),
+                typeof(IgbGridColumnOptionsModule)
+            );
+
+            await builder.Build().RunAsync();
+        }
+    }
+}
