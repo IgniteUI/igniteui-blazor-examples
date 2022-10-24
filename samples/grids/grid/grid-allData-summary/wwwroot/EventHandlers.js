0e911ff4c81cba808937f125c2e3669997110a38
Maya Kirova <MKirova@infragistics.com>
2022-10-24 17:54:55 +0300

    Add grid-allData-summary sample.

---
+++ samples/grids/grid/grid-allData-summary/wwwroot/EventHandlers.js
@@ -0,0 +1,34 @@
+﻿class DiscontinuedSummary {
+    operate(data, allData, fieldName) {
+        const discontinuedData = allData.filter((rec) => rec['Discontinued']).map(r => r[fieldName]);
+        const result = [];
+        result.push({
+            key: 'products',
+            label: 'Producs',
+            summaryResult: data.length
+        });
+        result.push({
+            key: 'total',
+            label: 'Total Items',
+            summaryResult: data.length ? data.reduce((a, b) => +a + +b) : 0
+        });
+        result.push({
+            key: 'discontinued',
+            label: 'Discontinued Producs',
+            summaryResult: allData.map(r => r['Discontinued']).filter((rec) => rec).length
+        });
+        result.push({
+            key: 'totalDiscontinued',
+            label: 'Total Discontinued Items',
+            summaryResult: discontinuedData.length ? discontinuedData.reduce((a, b) => +a + +b) : 0
+        });
+        return result;
+    }
+}
+function OnColumnInit(evt) {
+    if (evt.detail.field === "UnitsInStock") {
+        event.detail.summaries = DiscontinuedSummary;
+    }
+}
+ 
+igRegisterScript("OnColumnInit", OnColumnInit, false);
