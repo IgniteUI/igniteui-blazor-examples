72b1f7286298956a4ebe4a76ee01f9506d7963e0
HUSSAR-mtrela <martin.trela@gmail.com>
2022-10-28 14:31:43 -0400

    exported events.js files

---
+++ samples/grids/grid/column-pinning-options/wwwroot/events.js
@@ -0,0 +1,18 @@
+// this file contains events handlers and templates used in the razor page
+
+igRegisterScript("WebGridPinHeaderTemplate", (ctx) => {
+    var html = window.igTemplating.html;
+    window.toggleColumnPin = function toggleColumnPin(field) {
+        var col = grid.getColumnByName(field);
+        col.pinned = !col.pinned;
+        grid.markForCheck();
+    }
+    return html`<div>
+    <span style="float:left">${ctx.column.field}</span>
+    <span style="float:right" onclick='toggleColumnPin("${ctx.column.field}")'>📌</span>
+</div>`;
+}, false);
+
+
+//insert eventScript
+//end eventScript
