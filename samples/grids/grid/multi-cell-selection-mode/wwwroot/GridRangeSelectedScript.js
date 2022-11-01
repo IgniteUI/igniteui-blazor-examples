88115618d3732c6f2b501e62a6743bc7af85e7f2
Michael DiFilippo <mdifilippo@infragistics.com>
2022-11-01 19:41:26 -0400

    mdd-fix-in-JS
    getSelectedData works in JS only.

---
+++ samples/grids/grid/multi-cell-selection-mode/wwwroot/GridRangeSelectedScript.js
@@ -0,0 +1,7 @@
+﻿function RangeSelectionChanging(o, e) {
+    var g1 = document.querySelector("#grid1");
+    var g2 = document.querySelector("#grid2");
+    g2.data = g1.getSelectedData();
+}
+
+igRegisterScript("RangeSelectionChanging", RangeSelectionChanging, false);
