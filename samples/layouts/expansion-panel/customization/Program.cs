eb84c31212e01c08d883f33995d317908f2347cf
Robert Stoffers <rstoffers@infragistics.com>
2022-11-02 15:39:20 -0400

    add ExpansionPanel samples

---
+++ samples/layouts/expansion-panel/customization/Program.cs
@@ -0,0 +1,14 @@
+using BlazorClientApp;
+using IgniteUI.Blazor.Controls;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+builder.Services.AddIgniteUIBlazor(typeof(IgbButtonModule), typeof(IgbExpansionPanelModule));
+
+await builder.Build().RunAsync();
