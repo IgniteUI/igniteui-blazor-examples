d619a98ccc74239ca5ef99a226edbb9ad3781ae1
HUSSAR-mtrela <martin.trela@gmail.com>
2023-03-02 15:47:10 -0500

    exported CC fixes

--- samples/charts/category-chart/annotations-callouts/TemperatureAnnotatedData.cs
+++ samples/charts/category-chart/annotations-callouts/TemperatureAnnotatedData.cs
@@ -2,11 +2,12 @@ using System;
 using System.Collections.Generic;
 public class TemperatureAnnotatedDataItem
 {
-    public double Index{ get; set; }
-    public double Temperature{ get; set; }
-    public string TempInfo{ get; set; }
-    public string Month{ get; set; }
+    public double Index { get; set; }
+    public string TempInfo { get; set; }
+    public double Temperature { get; set; }
+    public string Month { get; set; }
 }
+
 public class TemperatureAnnotatedData
     : List<TemperatureAnnotatedDataItem>
 {
@@ -15,85 +16,85 @@ public class TemperatureAnnotatedData
         this.Add(new TemperatureAnnotatedDataItem()
         {
             Index = 0,
+            TempInfo = @"27�C",
             Temperature = 27,
-            TempInfo = @"27°C",
             Month = @"Jan"
         });
         this.Add(new TemperatureAnnotatedDataItem()
         {
             Index = 1,
+            TempInfo = @"25�C",
             Temperature = 25,
-            TempInfo = @"25°C",
             Month = @"Feb"
         });
         this.Add(new TemperatureAnnotatedDataItem()
         {
             Index = 2,
+            TempInfo = @"21�C",
             Temperature = 21,
-            TempInfo = @"21°C",
             Month = @"Mar"
         });
         this.Add(new TemperatureAnnotatedDataItem()
         {
             Index = 3,
+            TempInfo = @"19�C",
             Temperature = 19,
-            TempInfo = @"19°C",
             Month = @"Apr"
         });
         this.Add(new TemperatureAnnotatedDataItem()
         {
             Index = 4,
+            TempInfo = @"16�C",
             Temperature = 16,
-            TempInfo = @"16°C",
             Month = @"May"
         });
         this.Add(new TemperatureAnnotatedDataItem()
         {
             Index = 5,
+            TempInfo = @"13�C",
             Temperature = 13,
-            TempInfo = @"13°C",
             Month = @"Jun"
         });
         this.Add(new TemperatureAnnotatedDataItem()
         {
             Index = 6,
+            TempInfo = @"14�C",
             Temperature = 14,
-            TempInfo = @"14°C",
             Month = @"Jul"
         });
         this.Add(new TemperatureAnnotatedDataItem()
         {
             Index = 7,
+            TempInfo = @"15�C",
             Temperature = 15,
-            TempInfo = @"15°C",
             Month = @"Aug"
         });
         this.Add(new TemperatureAnnotatedDataItem()
         {
             Index = 8,
+            TempInfo = @"19�C",
             Temperature = 19,
-            TempInfo = @"19°C",
             Month = @"Sep"
         });
         this.Add(new TemperatureAnnotatedDataItem()
         {
             Index = 9,
+            TempInfo = @"22�C",
             Temperature = 22,
-            TempInfo = @"22°C",
             Month = @"Oct"
         });
         this.Add(new TemperatureAnnotatedDataItem()
         {
             Index = 10,
+            TempInfo = @"26�C",
             Temperature = 26,
-            TempInfo = @"26°C",
             Month = @"Nov"
         });
         this.Add(new TemperatureAnnotatedDataItem()
         {
             Index = 11,
+            TempInfo = @"30�C",
             Temperature = 30,
-            TempInfo = @"30°C",
             Month = @"Dec"
         });
     }
