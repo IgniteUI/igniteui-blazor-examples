d1c1c697a62afb19bc0224a06043afee1415a0c8
mddifilippo89 <mdifilippo@infragistics.com>
2023-03-21 12:48:44 -0400

    mdd-tree-grid-updates 2
    tree grid updates 2

---
+++ samples/grids/tree-grid/clipboard-operations/wwwroot/events.js
@@ -0,0 +1,13 @@
+
+
+igRegisterScript("WebGridClearSelection", (event) => {
+    console.log("TODO WebGridClearSelection");
+}, false);
+
+
+igRegisterScript("WebGridClipboardOperationsColumnInit", (e) => {
+    var column = e.detail;
+    column.formatter = columnFormatter;
+    column.header = "🎉" + column.field;
+}, false);
+
