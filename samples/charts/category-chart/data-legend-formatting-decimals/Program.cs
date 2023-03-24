fccbefcd4ad806217fb29b83addac301726a9614
HUSSAR-mtrela <martin.trela@gmail.com>
2023-03-24 18:09:18 -0400

    export modules

--- samples/charts/category-chart/data-legend-formatting-decimals/Program.cs
+++ samples/charts/category-chart/data-legend-formatting-decimals/Program.cs
@@ -24,7 +24,7 @@ namespace Infragistics.Samples
 
             // registering Infragistics Blazor
             builder.Services.AddIgniteUIBlazor(
-                typeof(IgbLegendModule),
+                typeof(IgbDataLegendModule),
                 typeof(IgbCategoryChartModule)
             );
 
