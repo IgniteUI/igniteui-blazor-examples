b10ca8595818dba6433f80c3025bd5cead098f2d
mddifilippo89 <mdifilippo@infragistics.com>
2023-03-24 13:10:22 -0400

    mdd-update-tree-data
    mdd-update-tree-data

--- samples/grids/tree-grid/editing-events/wwwroot/events.js
+++ samples/grids/tree-grid/editing-events/wwwroot/events.js
@@ -1,12 +1,12 @@
 
 
-igRegisterScript("WebGridEditingEventsCellEdit", (ev) => {
+igRegisterScript("WebTreeGridEditingEventsCellEdit", (ev) => {
     var d = ev.detail;
 
-    if (d.column != null && d.column.field == "UnitsOnOrder") {
-        if (d.newValue > d.rowData.UnitsInStock) {
+    if (d.column != null && d.column.field == "Name") {
+        if (d.newValue !== d.rowData.Name) {
             d.cancel = true;
-            alert("You cannot order more than the units in stock!")
+            alert("You cannot change the 'Name' field for this record!")
         }
     }
 }, false);
