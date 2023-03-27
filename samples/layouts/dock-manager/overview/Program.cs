2d21fb18baefaba467aacc52c92a55ef13884410
HUSSAR-mtrela <martin.trela@gmail.com>
2023-03-27 14:28:09 -0400

    fixed DM sample

--- samples/layouts/dock-manager/overview/Program.cs
+++ samples/layouts/dock-manager/overview/Program.cs
@@ -18,7 +18,8 @@ namespace Infragistics.Samples
             builder.RootComponents.Add<App>("app");
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
             // registering Ignite UI modules
-            builder.Services.AddIgniteUIBlazor(typeof(IgbDockManagerModule));            await builder.Build().RunAsync();
+            builder.Services.AddIgniteUIBlazor(typeof(IgbDockManagerModule));
+            await builder.Build().RunAsync();
         }
     }
 }
