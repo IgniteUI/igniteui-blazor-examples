276f8f0d651dd21192055165c98da1b8129e0264
HUSSAR-mtrela <martin.trela@gmail.com>
2023-03-28 17:42:25 -0400

    export grid fixes

--- samples/grids/grid/column-pinning-right-side/wwwroot/events.js
+++ samples/grids/grid/column-pinning-right-side/wwwroot/events.js
@@ -2,7 +2,11 @@
 igRegisterScript("WebGridImageCellTemplate", (ctx) => {
     var html = window.igTemplating.html;
     return html`<div>
-    <img src="${ctx.cell.value}"/>
+    <img src="${ctx.cell.value}"
+    style="border: 1px solid black;
+    object-fit: fill;
+    height: 2rem;
+    width: 3rem;"/>
  </div>`;
 }, false);
 
--- samples/grids/grid/row-paging-basic/wwwroot/events.js
+++ samples/grids/grid/row-paging-basic/wwwroot/events.js
@@ -1,7 +1,7 @@
 
 igRegisterScript("WebGridProgressCellTemplate", (ctx) => {
     var html = window.igTemplating.html;
-    return html`<div>
+    return html`<div style="width: 4rem">
     <igc-linear-progress value="${ctx.cell.value}"></igc-linear-progress>
  </div>`;
 }, false);
@@ -9,7 +9,11 @@ igRegisterScript("WebGridProgressCellTemplate", (ctx) => {
 igRegisterScript("WebGridImageCellTemplate", (ctx) => {
     var html = window.igTemplating.html;
     return html`<div>
-    <img src="${ctx.cell.value}"/>
+    <img src="${ctx.cell.value}"
+    style="border: 1px solid black;
+    object-fit: fill;
+    height: 2rem;
+    width: 3rem;"/>
  </div>`;
 }, false);
 
