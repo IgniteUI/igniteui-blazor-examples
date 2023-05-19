f12797c1a5cd0c625501213863f97e530476102a
tfsbuild <tfsbuild@infragistics.com>
2023-05-19 10:12:07 -0400

    Adding changes from build igniteui-xplat-examples-output+PRs_2023.5.19.1

--- samples/grids/pivot-grid/features/wwwroot/events.js
+++ samples/grids/pivot-grid/features/wwwroot/events.js
@@ -1,4 +1,5 @@
 
+
 igRegisterScript("PivotDataFlatAggregateSumSale", (members, data) => {
     return data.reduce((accumulator, value) => accumulator + value.ProductUnitPrice * value.NumberOfUnits, 0);
 }, false);
