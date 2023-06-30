de42416368a068a08459c3dbd420792536cc4627
mddifilippo89 <mdifilippo@infragistics.com>
2023-06-30 12:43:49 -0400

    update 2
    update 2

--- samples/grids/grid/binding-remote-data/Services/ProductDataService.cs
+++ samples/grids/grid/binding-remote-data/Services/ProductDataService.cs
@@ -15,21 +15,39 @@ namespace Infragistics.Samples
             _http = http;
         }
 
-        public async Task<List<NwindDataItem>> GetTable()
+        public async Task<List<ProductDataItem>> GetTable()
         {
             using HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Get, new Uri("https://www.igniteui.com/api/products", UriKind.RelativeOrAbsolute));
             using HttpResponseMessage response = await _http.SendAsync(request).ConfigureAwait(false);
             if (response.IsSuccessStatusCode)
             {
-                return await response.Content.ReadFromJsonAsync<List<NwindDataItem>>().ConfigureAwait(false);
+                return await response.Content.ReadFromJsonAsync<List<ProductDataItem>>().ConfigureAwait(false);
             }
 
-            return new List<NwindDataItem>();
+            return new List<ProductDataItem>();
         }
     }
 
     public interface IProductDataService
     {
-        Task<List<NwindDataItem>> GetTable();
+        Task<List<ProductDataItem>> GetTable();
+    }
+
+    public class ProductDataItem
+    {
+        public int ID { get; set; }
+        public string ProductName { get; set; }
+        public double SupplierID { get; set; }
+        public string SupplierName { get; set; }
+        public double CategoryID { get; set; }
+        public string CategoryName { get; set; }
+        public string QuantityPerUnit { get; set; }
+        public double UnitPrice { get; set; }
+        public double UnitsInStock { get; set; }
+        public double UnitsOnOrder { get; set; }
+        public double ReorderLevel { get; set; }
+        public bool Discontinued { get; set; }
+        public string OrderDate { get; set; }
+        public double Rating { get; set; }
     }
 }
