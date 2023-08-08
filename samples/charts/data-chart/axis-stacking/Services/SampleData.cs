9fa3850e166076a899827138e7643f9a172107d2
HUSSAR-mtrela (Martin Trela) <HUSSAR-mtrela@users.noreply.github.com>
2023-08-08 11:00:49 -0400

    add axis stacking (#440)

---
+++ samples/charts/data-chart/axis-stacking/Services/SampleData.cs
@@ -0,0 +1,122 @@
+using System.Collections.Generic;
+
+public class LocalDataItem
+{
+    public double Year { get; set; }
+    public double Income { get; set; }
+    public double Cashflow { get; set; }
+    public double Revenue { get; set; }
+}
+
+public class LocalData
+    : List<LocalDataItem>
+{
+    public LocalData()
+    {
+        this.Add(new LocalDataItem()
+        {
+            Year = 2009,
+            Income = 4.5,
+            Cashflow = 4,
+            Revenue = 50
+        });
+        this.Add(new LocalDataItem()
+        {
+            Year = 2010,
+            Income = 3,
+            Cashflow = 5,
+            Revenue = 70
+        });
+        this.Add(new LocalDataItem()
+        {
+            Year = 2011,
+            Income = 4.5,
+            Cashflow = 4,
+            Revenue = 80
+        });
+        this.Add(new LocalDataItem()
+        {
+            Year = 2012,
+            Income = 4,
+            Cashflow = 5,
+            Revenue = 90
+        });
+        this.Add(new LocalDataItem()
+        {
+            Year = 2013,
+            Income = 3.5,
+            Cashflow = 5,
+            Revenue = 85
+        });
+        this.Add(new LocalDataItem()
+        {
+            Year = 2014,
+            Income = 4,
+            Cashflow = 6,
+            Revenue = 90
+        });
+        this.Add(new LocalDataItem()
+        {
+            Year = 2015,
+            Income = 3.5,
+            Cashflow = 7,
+            Revenue = 80
+        });
+        this.Add(new LocalDataItem()
+        {
+            Year = 2016,
+            Income = 4,
+            Cashflow = 6,
+            Revenue = 95
+        });
+        this.Add(new LocalDataItem()
+        {
+            Year = 2017,
+            Income = 5.5,
+            Cashflow = 5,
+            Revenue = 85
+        });
+        this.Add(new LocalDataItem()
+        {
+            Year = 2018,
+            Income = 6.5,
+            Cashflow = 6,
+            Revenue = 90
+        });
+        this.Add(new LocalDataItem()
+        {
+            Year = 2019,
+            Income = 7,
+            Cashflow = 7.5,
+            Revenue = 95
+        });
+        this.Add(new LocalDataItem()
+        {
+            Year = 2020,
+            Income = 2.5,
+            Cashflow = 4,
+            Revenue = 65
+        });
+        this.Add(new LocalDataItem()
+        {
+            Year = 2021,
+            Income = 4.5,
+            Cashflow = 5,
+            Revenue = 70
+        });
+        this.Add(new LocalDataItem()
+        {
+            Year = 2022,
+            Income = 5,
+            Cashflow = 6,
+            Revenue = 85
+        });
+        this.Add(new LocalDataItem()
+        {
+            Year = 2023,
+            Income = 6.5,
+            Cashflow = 7,
+            Revenue = 95
+        });
+    }
+}
