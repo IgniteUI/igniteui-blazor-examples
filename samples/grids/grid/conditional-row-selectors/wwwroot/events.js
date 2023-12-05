90d3e66a217f01292c32c3ee13e5a49d21fb9947
tfsbuild <tfsbuild@infragistics.com>
2023-12-05 23:44:26 +0200

    Adding changes from build igniteui-xplat-examples-output+PRs_2023.12.5.1

--- samples/grids/grid/conditional-row-selectors/wwwroot/events.js
+++ samples/grids/grid/conditional-row-selectors/wwwroot/events.js
@@ -1,23 +1,22 @@
 
 
 igRegisterScript("WebGridRowSelectionConditional", (event) => {
-        const eventArgs = event.detail;
-        if (!eventArgs.added.length && eventArgs.removed.length) {
-            // ignore deselect
-            return;
-        }
-        const originalAddedLength = eventArgs.added.length;
+    const eventArgs = event.detail;
+    if (!eventArgs.added.length && eventArgs.removed.length) {
+        // ignore deselect
+        return;
+    }
+    const originalAddedLength = eventArgs.added.length;
 
-        // only allow selection of items that contain 'A'
-        eventArgs.newSelection = eventArgs.newSelection.filter(x => x.indexOf('A') !== -1);
+    // only allow selection of items that contain 'A'
+    eventArgs.newSelection = eventArgs.newSelection.filter(x => x["ID"].indexOf("A") !== -1);
 
-        // cleanup selection if all conditionally selectable rows are already selected
-        if (eventArgs.newSelection.length
-            && !eventArgs.newSelection.filter(x => eventArgs.oldSelection.indexOf(x) === -1).length
-            && originalAddedLength > 1) {
-                // all selected from header, deselect instead
-                eventArgs.newSelection = [];
-        }
-        grid.markForCheck();
+    // cleanup selection if all conditionally selectable rows are already selected
+    if (eventArgs.newSelection.length
+        && !eventArgs.newSelection.filter(x => eventArgs.oldSelection.indexOf(x) === -1).length
+        && originalAddedLength > 1) {
+            // all selected from header, deselect instead
+            eventArgs.newSelection = [];
+    }
 }, false);
 
