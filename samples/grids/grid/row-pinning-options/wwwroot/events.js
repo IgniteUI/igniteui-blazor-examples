b829191eb744ab48a1dab7d268e18f164b78bdcc
tfsbuild <tfsbuild@infragistics.com>
2024-03-28 04:56:56 -0400

    Adding changes from build igniteui-xplat-examples-output+PRs_2024.3.28.1

--- samples/grids/grid/row-pinning-options/wwwroot/events.js
+++ samples/grids/grid/row-pinning-options/wwwroot/events.js
@@ -7,10 +7,3 @@ igRegisterScript("WebGridSetRowPinning", (sender, evtArgs) => {
     grid.pinning.rows = newVal === "Top" ? 0 : 1;
 }, false);
 
-igRegisterScript("WebTreeGridPinRowOnRendered", (event) => {
-        var treeGrid = document.getElementById("treeGrid") || document.getElementById("grid");
-        treeGrid.data = [...treeGrid.data];
-        treeGrid.pinRow(1);
-        treeGrid.pinRow(11);
-}, false);
-
