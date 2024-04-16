b30bb3a1e7b6cfc17a17954abf506079791a1990
Ivan Kitanov <69432826+IvanKitanov17@users.noreply.github.com>
2024-04-16 15:07:39 +0300

    Add hGrid custom row-drag-base sample (#653)
    * Add hGrid custom row-drag-base sample
    
    * Update IgniteUI for Blazor version
    
    ---------
    
    Co-authored-by: Ivan Kitanov <IKitanov@infragistics.com>

---
+++ samples/grids/hierarchical-grid/row-drag-base/wwwroot/GridRowDragBaseEvents.js
@@ -0,0 +1,12 @@
+﻿function OnRowDragEndHandler(evt) {    
+    const ghostElement = evt.detail.dragDirective.ghostElement;
+    const dragElementPos = ghostElement.getBoundingClientRect();
+    const gridPosition = hierarchicalGrid2.getBoundingClientRect();
+    const withinXBounds = dragElementPos.x >= gridPosition.x && dragElementPos.x <= gridPosition.x + gridPosition.width;
+    const withinYBounds = dragElementPos.y >= gridPosition.y && dragElementPos.y <= gridPosition.y + gridPosition.height;
+    if (withinXBounds && withinYBounds) {        
+        hierarchicalGrid2.addRow(evt.detail.dragData.data);         
+    }
+}
+
+igRegisterScript("OnRowDragEndHandler", OnRowDragEndHandler, false);
