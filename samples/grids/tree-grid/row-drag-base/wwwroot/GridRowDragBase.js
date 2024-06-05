54219f25f75b89721ff93feb26d8831e53dd149b
Ivan Kitanov <IKitanov@infragistics.com>
2024-06-05 08:10:37 +0300

    Modyfind the addition of rows so that no row is added more than once

--- samples/grids/tree-grid/row-drag-base/wwwroot/GridRowDragBase.js
+++ samples/grids/tree-grid/row-drag-base/wwwroot/GridRowDragBase.js
@@ -1,3 +1,26 @@
+function addRowAndChildren(row, newData) {
+    if (newData.includes(row)) {
+        return;
+    }
+    else if (newData.length > 0 && row.Employees) {
+        for (let i = row.Employees.length; i >= 0; i--) {
+            if (newData.includes(row.Employees[i])) {
+                let index = newData.findIndex(element => element.ID === row.Employees[i].ID);
+                if (index > -1) {
+                    newData.splice(index, 1);
+                }
+            }
+        }
+    }
+
+    for (let record of newData) {
+        if (record.Employees && record.Employees.includes(row)) {
+            return;
+        }
+    }
+
+    newData.push(row);
+}
 function OnRowDragEndHandler(evt) {
     const ghostElement = evt.detail.dragDirective.ghostElement;
     const dragElementPos = ghostElement.getBoundingClientRect();
@@ -5,7 +28,10 @@ function OnRowDragEndHandler(evt) {
     const withinXBounds = dragElementPos.x >= gridPosition.x && dragElementPos.x <= gridPosition.x + gridPosition.width;
     const withinYBounds = dragElementPos.y >= gridPosition.y && dragElementPos.y <= gridPosition.y + gridPosition.height;
     if (withinXBounds && withinYBounds) {
-        treeGrid2.addRow(evt.detail.dragData.data);
+        const newData = [...treeGrid2.data];
+        const draggedRowData = evt.detail.dragData.data;
+        addRowAndChildren(draggedRowData, newData);
+        treeGrid2.data = newData;
     }
 }
 
