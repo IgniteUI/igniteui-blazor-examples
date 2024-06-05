7bea4154c5464595ad64695e1bd056af5dab35dd
tfsbuild <tfsbuild@infragistics.com>
2024-06-05 15:05:02 +0300

    Adding changes from build igniteui-xplat-examples-output+PRs_2024.6.5.4

---
+++ samples/grids/tree-grid/conditional-cell-style-2/wwwroot/events.js
@@ -0,0 +1,14 @@
+
+igRegisterScript("WebTreeGridCellStylesHandler", () => {
+    return {
+        background: (rowData, columnKey, cellValue, rowIndex) => rowIndex % 2 === 0 ? "#EFF4FD" : null,
+        color: (rowData, columnKey, cellValue, rowIndex) => {
+            if (columnKey === "UnitPrice") {
+                if (cellValue > 10) return "#dc3545";
+                if (cellValue < 5) return "#28a745";
+                if (cellValue >= 5 && cellValue <= 10) return "#17a2b8";
+            }
+        }
+    };
+}, true);
+
