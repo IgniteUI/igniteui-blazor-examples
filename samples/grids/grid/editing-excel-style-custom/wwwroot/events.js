c7c37f520a9ffcb66a745b5549c6b678a7f7927b
ttonev <ttonev@infragistics.com>
2024-08-06 10:40:17 +0300

    docs(grid): updated sample

--- samples/grids/grid/editing-excel-style-custom/wwwroot/events.js
+++ samples/grids/grid/editing-excel-style-custom/wwwroot/events.js
@@ -4,28 +4,31 @@ igRegisterScript("WebGridEditingExcelStyle", (ev) => {
 
     // Attach the onkeydown event listener to the gridElement or a specific child element
     gridElement.addEventListener('keydown', (keydownEvent) => {
-        // Handle the keydown event
-        var key = keydownEvent.keyCode; // or `keydownEvent.which` for wider browser compatibility
-        console.log(`Key pressed: ${key}`);
 
-        // You can include your logic here, or call other functions as needed
-    });
+    }, false);
 
-    // The rest of your existing logic...
-}, false);
-
-function getNextEditableRowIndex(currentRowIndex, dataView, previous) {
-    if (currentRowIndex < 0 || (currentRowIndex === 0 && previous) || (currentRowIndex >= dataView.length - 1 && !previous)) {
-        return currentRowIndex;
+    function getNextEditableRowIndex(currentRowIndex, dataView, previous) {
+        if (currentRowIndex < 0 || (currentRowIndex === 0 && previous) || (currentRowIndex >= dataView.length - 1 && !previous)) {
+            return currentRowIndex;
+        }
+        if (previous) {
+            return dataView.findLastIndex((rec, index) => index < currentRowIndex && this.isEditableDataRecordAtIndex(index, dataView));
+        }
+        return dataView.findIndex((rec, index) => index > currentRowIndex && this.isEditableDataRecordAtIndex(index, dataView));
     }
-    if (previous) {
-        return dataView.findLastIndex((rec, index) => index < currentRowIndex && this.isEditableDataRecordAtIndex(index, dataView));
-    }
-    return dataView.findIndex((rec, index) => index > currentRowIndex && this.isEditableDataRecordAtIndex(index, dataView));
-}
 
-function isEditableDataRecordAtIndex(dataViewIndex, dataView) {
-    const rec = dataView[dataViewIndex];
-    return !rec.expression && !rec.summaries && !rec.childGridsData && !rec.detailsData;
-}
+    function isEditableDataRecordAtIndex(dataViewIndex, dataView) {
+        const rec = dataView[dataViewIndex];
+        return !rec.expression && !rec.summaries && !rec.childGridsData && !rec.detailsData;
+    }
+
+    function preventDefaultForKeyEvent(event) {
+        event.preventDefault();
+    }
+
+    function getBodyFocus() {
+        var element = document.getElementsByClassName("igx-grid__tbody-content")[0];
+        if (element) {
+            element.focus();
+        }
 
