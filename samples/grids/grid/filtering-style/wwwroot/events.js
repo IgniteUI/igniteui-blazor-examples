d7a2e34e0a9782e096783a1b407ee2664d8a5d08
tfsbuild <tfsbuild@infragistics.com>
2024-08-14 00:47:37 +0300

    Adding changes from build igniteui-xplat-examples-output+PRs_2024.8.13.2

--- samples/grids/grid/filtering-style/wwwroot/events.js
+++ samples/grids/grid/filtering-style/wwwroot/events.js
@@ -8,3 +8,9 @@ igRegisterScript("WebGridBooleanCellTemplate", (ctx) => {
     }
 }, false);
 
+igRegisterScript("WebGridSetGridSize", (sender, evtArgs) => {
+    var newVal = evtArgs.newValue.toLowerCase();
+    var grid = document.getElementById("grid");
+    grid.style.setProperty('--ig-size', `var(--ig-size-${newVal})`);
+}, false);
+
