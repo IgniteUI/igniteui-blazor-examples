331860d3831a4d1705f7007c37e392010f56c6cc
tfsbuild <tfsbuild@infragistics.com>
2024-08-27 12:10:34 +0300

    Adding changes from build igniteui-xplat-examples-output+PRs_2024.8.27.1

--- samples/grids/tree-grid/excel-style-filtering-sample-1/wwwroot/events.js
+++ samples/grids/tree-grid/excel-style-filtering-sample-1/wwwroot/events.js
@@ -8,3 +8,9 @@ igRegisterScript("WebGridBooleanCellTemplate", (ctx) => {
     }
 }, false);
 
+igRegisterScript("WebTreeGridSetGridSize", (sender, evtArgs) => {
+    var newVal = evtArgs.newValue.toLowerCase();
+    var grid = document.getElementById("treeGrid");
+    grid.style.setProperty('--ig-size', `var(--ig-size-${newVal})`);
+}, false);
+
