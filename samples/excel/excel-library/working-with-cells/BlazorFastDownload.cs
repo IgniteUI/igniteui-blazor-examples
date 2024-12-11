2eb28bcc090e3b27609e880856fe701322d30d5d
Valerie Suszynski <vsuszynski@infragistics.com>
2024-12-11 16:51:51 -0500

    switch from using InvokeUnmarshalled to JSImport for downloading excel files in WASM

---
+++ samples/excel/excel-library/working-with-cells/BlazorFastDownload.cs
@@ -0,0 +1,11 @@
+﻿using System.Runtime.InteropServices.JavaScript;
+
+namespace Infragistics.Samples
+{
+    public partial class BlazorFastDownload
+    {
+        [JSImport("BlazorDownloadFileFast", "BlazorFastDownload")]
+        internal static partial void DownloadFile(string name, string contentType, byte[] content);
+
+    }
+}
