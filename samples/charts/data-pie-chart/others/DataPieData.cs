9b5b192e6b6d4d76c3bf37f24f029669e2d79b5e
tfsbuild <tfsbuild@infragistics.com>
2025-02-08 05:02:22 +0200

    Adding changes from build igniteui-xplat-examples-output+PRs_2025.2.8.1

--- samples/charts/data-pie-chart/others/DataPieData.cs
+++ samples/charts/data-pie-chart/others/DataPieData.cs
@@ -1,52 +1,52 @@
+using System;
 using System.Collections.Generic;
-
-public class LocalDataItem
+public class DataPieDataItem
 {
     public double V1 { get; set; }
     public string Category { get; set; }
 }
 
-public class LocalData
-    : List<LocalDataItem>
+public class DataPieData
+    : List<DataPieDataItem>
 {
-    public LocalData()
+    public DataPieData()
     {
-        this.Add(new LocalDataItem()
+        this.Add(new DataPieDataItem()
         {
             V1 = 100,
             Category = @"Maintenance"
         });
-        this.Add(new LocalDataItem()
+        this.Add(new DataPieDataItem()
         {
             V1 = 40,
             Category = @"Cooling"
         });
-        this.Add(new LocalDataItem()
+        this.Add(new DataPieDataItem()
         {
             V1 = 20,
             Category = @"Residential"
         });
-        this.Add(new LocalDataItem()
+        this.Add(new DataPieDataItem()
         {
             V1 = 15,
             Category = @"Utilities"
         });
-        this.Add(new LocalDataItem()
+        this.Add(new DataPieDataItem()
         {
             V1 = 10,
             Category = @"Heating"
         });
-        this.Add(new LocalDataItem()
+        this.Add(new DataPieDataItem()
         {
             V1 = 5,
             Category = @"Lighting"
         });
-        this.Add(new LocalDataItem()
+        this.Add(new DataPieDataItem()
         {
             V1 = 4,
             Category = @"Electric"
         });
-        this.Add(new LocalDataItem()
+        this.Add(new DataPieDataItem()
         {
             V1 = 3,
             Category = @"Misc"
