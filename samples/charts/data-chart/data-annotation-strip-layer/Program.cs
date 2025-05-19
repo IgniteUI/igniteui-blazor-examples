a0f3f0fdfe61047403c4a29a58cbb3f10999ee1d
tfsbuild <tfsbuild@infragistics.com>
2025-05-19 11:42:25 -0400

    Adding changes from build igniteui-xplat-examples-output+PRs_2025.5.19.1

---
+++ samples/charts/data-chart/data-annotation-strip-layer/Program.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Net.Http;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Text;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using IgniteUI.Blazor.Controls; // for registering Ignite UI modules
+
+namespace Infragistics.Samples
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+            builder.RootComponents.Add<App>("app");
+            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            // registering Ignite UI modules
+            builder.Services.AddIgniteUIBlazor(
+                typeof(IgbDataChartCoreModule),
+                typeof(IgbDataChartCategoryModule),
+                typeof(IgbDataChartCategoryCoreModule),
+                typeof(IgbDataChartFinancialCoreModule),
+                typeof(IgbDataChartFinancialModule),
+                typeof(IgbDataChartFinancialOverlaysModule),
+                typeof(IgbDataChartInteractivityModule),
+                typeof(IgbDataChartAnnotationModule),
+                typeof(IgbDataAnnotationStripLayerModule),
+                typeof(IgbNumberAbbreviatorModule),
+                typeof(IgbAnnotationLayerProxyModule)
+            );
+            await builder.Build().RunAsync();
+        }
+    }
+}
