186f272192a6649d53bfdd474ed1451358f3bb85
tfsbuild <tfsbuild@infragistics.com>
2025-06-02 20:19:40 +0300

    Adding changes from build igniteui-xplat-examples-output+PRs_2025.6.2.2

---
+++ samples/charts/data-chart/data-annotation-rect-layer/AnnotationRectData.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections.Generic;
+public class AnnotationRectDataItem
+{
+    public double StartX { get; set; }
+    public double StartY { get; set; }
+    public double EndX { get; set; }
+    public double EndY { get; set; }
+    public string Label { get; set; }
+}
+
+public class AnnotationRectData
+    : List<AnnotationRectDataItem>
+{
+    public AnnotationRectData()
+    {
+        this.Add(new AnnotationRectDataItem()
+        {
+            StartX = 85,
+            StartY = 190,
+            EndX = 140,
+            EndY = 415,
+            Label = @"Head & Shoulders Pattern
+  (Bearish Downtrend)"
+        });
+        this.Add(new AnnotationRectDataItem()
+        {
+            StartX = 53,
+            StartY = 75,
+            EndX = 230,
+            EndY = 80,
+            Label = @"Price Gap (Bearish Target)"
+        });
+    }
+}
