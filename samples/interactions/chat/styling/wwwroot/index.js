8bf4d2979420abdc8a392af2e7efc469cf38aeae
Maya <MKirova@infragistics.com>
2026-06-16 16:25:30 +0300

    New Chat samples. (#1271)
    * feat(chat): Add overview sample.
    
    * Add features sample.
    
    * Add styling sample.
    
    * Update to official version.
    
    * Add auto response to chat overview sample.

---
+++ samples/interactions/chat/styling/wwwroot/index.js
@@ -0,0 +1,16 @@
+﻿igRegisterScript("OnMessageHeaderTemplate", (ctx) => {
+    var html = window.igTemplating.html;
+    return ctx.message.sender !== 'user'
+        ? html`
+          <div style="display: flex; align-items: center; gap: 8px;">
+            <igc-avatar
+              shape="circle"
+              src="https://dl.infragistics.com/x/img/people/men/11.png"
+              style="position: relative;"
+            >
+            </igc-avatar>
+            <span>Customer Support</span>
+          </div>
+        `
+        : "";
+}, false);
