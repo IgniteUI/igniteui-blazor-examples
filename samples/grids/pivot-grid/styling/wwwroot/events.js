2a3b31f474f57b7af350ed319b9948224d5cc545
Adrian Petrov <adrian.ptrv1@gmail.com>
2026-07-01 17:04:08 +0300

    feat(pivot-grid): add styling sample

---
+++ samples/grids/pivot-grid/styling/wwwroot/events.js
@@ -0,0 +1,9 @@
+
+
+igRegisterScript("PivotSalesDataAggregateMaxSales", (members, data) => {
+    if (!data) {
+        return [];
+    }
+    return data.map(x => x.Sales).reduce((a, b) => Math.max(a, b));
+}, false);
+
